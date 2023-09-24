2bb40133b833d3929778ae7251fafcfc2e958698
Mauricio Robinson <marsjr97@gmail.com>
2023-09-24 15:07:00 -0400

    [FIX]: fixing connection to the DB

--- myApp.js
+++ myApp.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB');
+  } catch (error) {
+    console.error('Error while trying to connect to the DB', error.message);
+  }
+};
+
+connectDB();
 
 let Person;
 
